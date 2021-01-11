9cd05326582966ac6bcf28830fb6474b79220169
Abdu Maa <abdurahmanmaa@hotmail.de>
2021-01-11 20:12:43 +0100

    fixed issues with pytrend

--- gtrends.py
+++ gtrends.py
@@ -7,12 +7,18 @@ import seaborn as sns
 from pytrends.request import TrendReq
 
 # Create a pytrend object. Request data from Google. hl parameter specifies host-language.
-pytrends = TrendReq(hl='en-US') #, tz=360, retries=10, backoff_factor=0.5
+pytrends = TrendReq(hl='en-US', tz=360) #, tz=360, retries=10, backoff_factor=0.5
 
 # Daily Trend in US
 trending_searches_df = pytrends.trending_searches(pn='germany')
 print(trending_searches_df.head(20))
 
 # Extract data about keyword
-kw_list = ["hello"]
-pytrends.build_payload(kw_list, cat='0', timeframe='today 2-y', geo='', gprop='') #, timeframe='today 1-y'
+kw_list = ["anosmia", "depression", "vaccine"]
+pytrends.build_payload(kw_list, cat='0', timeframe='today 5-y', geo='US', gprop='')
+data = pytrends.interest_over_time()
+data.head()
+
+# Plot data
+data.plot(title='Anosmia, Depression and Vaccine Searches on Google for the last 5 years')
+plt.show
