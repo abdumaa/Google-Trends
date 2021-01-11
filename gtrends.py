43815d1a9ce4c4dfcaec6340d0c5a970c2d949d9
Abdu Maa <abdurahmanmaa@hotmail.de>
2021-01-11 09:44:45 +0100

    updated py

--- gtrends.py
+++ gtrends.py
@@ -9,6 +9,10 @@ from pytrends.request import TrendReq
 # Create a pytrend object. Request data from Google. hl parameter specifies host-language.
 pytrends = TrendReq(hl='en-US') #, tz=360, retries=10, backoff_factor=0.5
 
+# Daily Trend in US
+trending_searches_df = pytrends.trending_searches(pn='germany')
+print(trending_searches_df.head(20))
+
 # Extract data about keyword
-kw_list = ["anosmia"]
-pytrends.build_payload(kw_list, timeframe='today 1-y')
+kw_list = ["hello"]
+pytrends.build_payload(kw_list, cat='0', timeframe='today 2-y', geo='', gprop='') #, timeframe='today 1-y'
