2e3c3d90b107301d7c94b64d8a051b7b7449476f
Abdu Maa <abdurahmanmaa@hotmail.de>
2021-01-14 12:57:08 +0100

    updated pyscript

--- gtrends.py
+++ gtrends.py
@@ -9,13 +9,13 @@ from pytrends.request import TrendReq
 # Create a pytrend object. Request data from Google. hl parameter specifies host-language.
 pytrends = TrendReq(hl='en-US', tz=360) #, tz=360, retries=10, backoff_factor=0.5
 
-# Daily Trend in US
+# Daily Trend in Germany
 trending_searches_df = pytrends.trending_searches(pn='germany')
 print(trending_searches_df.head(20))
 
 # Extract data about keyword
-kw_list = ["lockdown", "vaccine", "homeschooling", "mortality"]
-pytrends.build_payload(kw_list, cat='0', timeframe='2019-01-01 2021-01-01', geo='US', gprop='')
+kw_list = ["lockdown", "vaccine", "unemployment", "zoom"]
+pytrends.build_payload(kw_list, cat='0', timeframe='2019-10-01 2021-01-01', geo='US', gprop='')
 data = pytrends.interest_over_time().drop(['isPartial'], axis = 1)
 data.head()
 
